8252bd95ac7372bf032113fb37877a54e7d396fc
GabbyYukio <gabbyyukio@gmail.com>
2022-01-19 15:03:57 +0200

    Some Changes

--- NumareMari/Bigger_Int.cs
+++ NumareMari/Bigger_Int.cs
@@ -103,7 +103,7 @@ namespace NumareMari
                     solutie.data[i] = x.data[xx - i] + carry;
                     carry = 0;
                 }
-                else if (x.data[xx] - y.data[yy] < 0)
+                else if (x.data[xx] - y.data[yy] + carry < 0)
                 {
                     solutie.data[i] = (10+x.data[xx-i])-y.data[yy-i] + carry;
                     carry = -1;
@@ -114,8 +114,8 @@ namespace NumareMari
                     carry = 0;
                 }
             }
-            solutie.Resizing(n);
             solutie.Reverse();
+            solutie.RemoveUnnecessaryZero();
             return solutie;
         }
         public static NumMare Multiplication_by_Digit(NumMare x, int a)//multiplica numarul x cu a<10 si il returneaza ca un nou obiect
@@ -125,7 +125,7 @@ namespace NumareMari
             int xx = x.data.Length, carry = 0;
             for(int i=0; i<xx; i++)
             {
-                if(x.data[xx-i]*a>10)
+                if(x.data[xx-i]*a+carry>10)
                 {
                     solutie.data[i] = (x.data[xx - i] * a) % 10+carry;
                     carry = (x.data[xx - i] * a) / 10;
@@ -137,45 +137,46 @@ namespace NumareMari
                 }
             }
             solutie.data[xx] = carry;
-            solutie.Resizing(xx);
             solutie.Reverse();
+            solutie.RemoveUnnecessaryZero();
             return solutie;
         }
         public static NumMare Multiplication_by_10Pow(NumMare x, int a)//multiplica NumMare cu 10 (accepta doar numere 10 la putere)
         {
             int n=x.data.Length;
-            NumMare solutie = new NumMare();
-
-            Copy(x, solutie);
+            NumMare solutie = x;
             while(a%10==0)
             {
-                solutie.data[n] = 0;
                 n++;
                 a /= 10;
             }
-            solutie.Resizing(n);
-
+            solutie.FillWithZero(n);
             return solutie;
         }
 
-        public static NumMare Multiplication_by_NumMare(NumMare x, NumMare y)
+        public static NumMare Multiplication_by_NumMare(NumMare x, NumMare y)//inmulteste NumMare x si NumMare y si returneaza tot NumMare
         {
             NumMare Solutie = new NumMare("0");
-            NumMare placeholder = new NumMare();
+            NumMare placeholder = new NumMare("0");
             int xx = x.data.Length, yy=y.data.Length, n=xx, sub, pow=0;
+            //inmultirea o facem de mana
             if (xx < yy)
             {
-                n = yy;
-                for (int i = 0; i < n; i++)
+                for (int i = 0; i < n; i++)//parcurgem elementele celui mai mic de la dreapta la stanga si le luam rand pe rand
                 {
+                    //retinem cifra curenta intr-o variabila de tip int (sub < 10!!!!!!!)
                     sub = x.data[xx - i];
+                    //inmultim variabila de tip int sub cu NumMare prin metoda definita
                     placeholder = Multiplication_by_Digit(y, sub);
+                    //calculam cu cate zero-uri trebuie sa adaugam la produs
                     pow = Convert.ToInt32(Math.Pow(10, i));
+                    //inmultim 
                     Solutie = Sum(Multiplication_by_10Pow(placeholder, pow), Solutie);
                 }
             }
             else
             {
+                n = yy;
                 for (int i = 0; i < n; i++)
                 {
                     sub = y.data[yy - i];
@@ -192,35 +193,30 @@ namespace NumareMari
         //:::::::::::::::::Proprietati pentru NumMare
         public void Reverse()//reverses the elements of MareNum
         {
-            int[] copie = new int[this.data.Length];
-
-            for(int i=data.Length; i>0;i--)
-                copie[data.Length - i] = this.data[i-1];
+            int[] copie = this.data;
             for (int i = 0; i < this.data.Length; i++)
-                this.data[i] = copie[i];
+                this.data[i] = copie[this.data.Length-i];
 
         }        
-        public void Resizing(int a) //reduces the array length up to a 
+        public void Resizing(int a) //reduce vectorul NumMare curent pana la variabila a
         {
-            
-            if(this.data[a-1]!=0)//checks if the first element of the array wont be 0
+
+            int[] copie = this.data;
+            this.data = new int[a];
+            for(int i = 0; i < a; i++)
+                this.data[i]=copie[i];
+        }
+
+        private void RemoveUnnecessaryZero()//sterge zero-urile de la inceputul unui numar
+        {
+            int i = 0;
+            while(this.data[i]!=0)
             {
-                int[] copie = new int[a];
-                for (int i=0; i<a; i++)
-                   copie[i]=this.data[i];
-                this.data = new int[a];
-                for (int i = 0; i < a; i++)
-                    this.data[i] = copie[i];
+                for (int j = i; j <this.data.Length - i; j++)
+                    this.data[j] = this.data[j + 1];
+                i++;
             }
-            else//if it is, it excludes it
-            {
-                int[] copie = new int[a-1];
-                for (int i=0; i<a-1; i++)
-                    copie[i] = this.data[i];
-                this.data = new int[a];
-                for (int i = 0; i < a - 1; i++)
-                    this.data[i] = copie[i];
-            }
+            this.Resizing(this.data.Length - i);
         }
         public void Print()//Afiseaza numarul ca string
         {
@@ -229,11 +225,12 @@ namespace NumareMari
             for(int i=0;  i<this.data.Length; i++)
                 Console.Write("{0}",this.data[i]);
         }
-        public static void Copy(NumMare x, NumMare y)//copiaza elementele din x in y, cu y avand intMaxValue ca si numar de elemente
+        private void FillWithZero(int a)
         {
-            y.data = new int[int.MaxValue];
-            for (int i = 0; i < x.data.Length; i++)
-                y.data[i] = x.data[i];
+            int[] copie = this.data;
+            this.data=new int[a];
+            for (int i = 0; i < copie.Length; i++)
+                this.data[i] = copie[i];
         }
     }
 }
